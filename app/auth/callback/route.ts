7e970d3e2e466f67c42820c52069ff5de16a0cd3
Bluejay29 <solvedsuite@gmail.com>
2026-02-16 00:47:39 -0500

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@/lib/supabase'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  if (code) {
+    const supabase = await createClient()
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  // This sends the user back to the homepage once they are logged in
+  return NextResponse.redirect(`${origin}/`)
+}
