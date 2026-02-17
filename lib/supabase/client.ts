aac4c99c64844e0d380e0d165b186c68882244a9
Bluejay29 <solvedsuite@gmail.com>
2026-02-16 22:00:27 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,14 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserClient } from '@supabase/ssr'
 
 // This version uses cookies to "remember" the user so you don't have to log in every time
-export const supabase = createClientComponentClient()
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
 
 // This allows other files to create their own connection if needed
-export const createClient = () => createClientComponentClient()
+export const createClient = () => 
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
